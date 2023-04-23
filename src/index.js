f8d61cc2926ebe93846980ac1a83d43d619c62f3
guilherme <guilhermg23@gmail.com>
2023-04-23 20:16:04 -0300

    feat: add cache by ip

--- src/index.js
+++ src/index.js
@@ -3,44 +3,69 @@ const cors = require('cors')
 const { join } = require('path')
 const { createClient } = require('redis')
 const { v4: uuid } = require('uuid')
+const ip = require('request-ip')
 
 require('dotenv').config({ path: join('..', '.env') })
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(ip.mw())
 
 const redis = createClient({
     url: process.env.REDIS_DB_URL,
     password: process.env.REDIS_DB_PASSWORD
 })
 
+const getIpAddress = (req) =>
+    req.connection.localAddress === req.connection.remoteAddress
+        ? 'localhost'
+        : ip.getClientIp()
+
 app.post('/', async (req, res) => {
     const { template } = req.body
+    const ipAddress = getIpAddress(req)
 
     if (!template || template.trim() === '')
         return res.json({ err: 'A template must be provided' })
 
+    const setKey = async (hash, key, value) => {
+        await redis.hSet(hash, key, value)
+        await redis.expire(hash, 60)
+    }
+
+    const data = await redis.hGetAll(ipAddress)
+    if (data) {
+        const cachedId = Object.keys(data)[0]
+        if (cachedId) {
+            await setKey(ipAddress, cachedId, template)
+            return res.json({ cachedId })
+        }
+    }
+
     const id = uuid()
-    await redis.set(id, template, { EX: 120 })
+    await setKey(ipAddress, id, template)
 
     return res.send({ id })
 })
 
 app.get('/view/:id', async (req, res) => {
     const { id } = req.params
+    const ipAddress = getIpAddress(req)
 
     if (!id || id.trim() === '')
         return res.json({ err: 'An id must be provided' })
 
-    const data = await redis.get(id)
-    if (!data) return res.json({ err: 'Content unavailable' })
+    const data = await redis.hGetAll(ipAddress)
+    const template = data[id]
 
-    return res.send(data)
+    if (!template) return res.json({ err: 'Content unavailable' })
+
+    return res.send(template)
 })
 
 const port = process.env.PORT || 3333
 
 redis.connect().then(() => {
-    app.listen(port, () => console.log(`Running server on ${port}`))
+    app.listen(port, '0.0.0.0', () => console.log(`Running server on ${port}`))
 })
