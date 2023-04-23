137244e3f89fd4f478761a0e29e2c9c0b3d7f30d
guilherme <guilhermg23@gmail.com>
2023-04-23 17:29:51 -0300

    fix and update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
 const express = require('express')
-const htmlToImage = require('node-html-to-image')
+const cors = require('cors')
+const { join } = require('path')
+const { createClient } = require('redis')
+const { v4: uuid } = require('uuid')
+
+require('dotenv').config({ path: join('..', '.env') })
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
-app.get('/', async (req, res) => {
-    const { template } = req.query
+const redis = createClient({
+    url: process.env.REDIS_DB_URL,
+    password: process.env.REDIS_DB_PASSWORD
+})
+
+app.post('/', async (req, res) => {
+    const { template } = req.body
 
     if (!template || template.trim() === '')
         return res.json({ err: 'A template must be provided' })
 
-    const image = await htmlToImage({ html: template })
-    res.writeHead(200, { 'Content-Type': 'image/jpeg' })
-    res.end(image, 'binary')
+    const id = uuid()
+    await redis.set(id, template, { EX: 120 })
+
+    return res.send({ id })
+})
+
+app.get('/view/:id', async (req, res) => {
+    const { id } = req.params
+
+    if (!id || id.trim() === '')
+        return res.json({ err: 'An id must be provided' })
+
+    const data = await redis.get(id)
+    if (!data) return res.json({ err: 'Content unavailable' })
+
+    return res.send(data)
 })
 
 const port = process.env.PORT || 3333
-app.listen(3333, () => console.log(`Running server on ${port}`))
+
+redis.connect().then(() => {
+    app.listen(port, () => console.log(`Running server on ${port}`))
+})
